e0c01c30b29d54c4dfa14a354448ea2f5c48253d
Udith-Madhav <udithmadhav@gmail.com>
2024-02-07 15:56:44 +0530

    07/02/2024

--- lib/12.dart
+++ lib/12.dart
@@ -6,5 +6,12 @@ void main(){
   stdout.write("Enter a character: ");
   String? letter=stdin.readLineSync();
 
-  if(letter=="a" || letter=="A" || letter=="")
+  if(letter=="a" || letter=="A" || letter=="e" || letter=="E" 
+  || letter=="i" || letter=="I" || letter=="o" || letter=="O"
+  || letter=="u" || letter=="U"){
+    print("$letter is vowel");
+  }
+  else{
+    print("$letter is consonant");
+  }
 }
